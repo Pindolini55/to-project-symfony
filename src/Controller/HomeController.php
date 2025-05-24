7b1327bb12d3c93f835ec3d813dfa8f565ade207
Pindolini55 <dawid-r07@wp.pl>
2025-05-24 10:34:07 +0200

    Przeniesienie headera do base.html.twig aby był globalnie dostępny, zmiana widoku listy produktów, dodane filtrowanie i sortowanie prbsłoduktów. Obsługa filtrowania i sortowania w ProductController.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,33 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'app_default')]
+    public function default(EntityManagerInterface $entityManager): Response
+    {
+        $qb = $entityManager->createQueryBuilder();
+        $qb->select('p.id, p.brand, p.model, p.price, p.description, p.imageUrl, p.colors, p.sizes,
+            COUNT(po.id) AS opinionsCount, AVG(po.value) AS averageRating')
+            ->from(ProductOpinion::class, 'po')
+            ->leftJoin('po.idProduct', 'p')
+            ->groupBy('po.idProduct')
+            ->having('COUNT(po.id) > 3')
+            ->andHaving('AVG(po.value) > 3')
+            ->addOrderBy('averageRating', 'DESC')
+            ->addOrderBy('opinionsCount', 'DESC')
+            ->setMaxResults(3);
+
+        $query = $qb->getQuery();
+        $popularProducts = $query->getResult();
+
+        foreach ($popularProducts as $key => $popularProduct) {
+            $popularProducts[$key]['rating'] = number_format($popularProduct['averageRating'], 1, '.', '') . ' (' . $popularProduct['opinionsCount'] . ')';
+        }
+
+        return $this->render('home/index.html.twig', [
+            'popularProducts' => $popularProducts
+        ]);
+    }
+    #[Route('/home', name: 'app_home')]
     public function index(EntityManagerInterface $entityManager): Response
     {
         $qb = $entityManager->createQueryBuilder();
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Entity\ProductOpinion;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,12 +17,44 @@ class ProductController extends AbstractController
      * Lista produktów
      */
     #[Route('/products', name: 'app_products_list')]
-    public function list(EntityManagerInterface $entityManager): Response
+    public function list(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $minPrice = $request->query->get('minPrice');
+        $maxPrice = $request->query->get('maxPrice');
+        $sizes = $request->query->all('size');
+        $colors = $request->query->all('color');
+        $sortBy = $request->query->get('sort');
         $qb = $entityManager->createQueryBuilder()
             ->select('p.id, p.brand, p.model, p.price, p.description, p.imageUrl, p.colors, p.sizes')
             ->from(Product::class, 'p')
         ;
+        if ($sortBy === 'price_asc') {
+            $qb->orderBy('p.price', 'ASC');
+        } else if ($sortBy === 'price_desc') {
+            $qb->orderBy('p.price', 'DESC');
+        }
+        if (!empty($minPrice) && is_numeric($minPrice)) {
+            $qb->andWhere('p.price >= :minPrice');
+            $qb->setParameter('minPrice', $minPrice);
+        }
+        if (!empty($maxPrice) && is_numeric($maxPrice)) {
+            $qb->andWhere('p.price <= :maxPrice');
+            $qb->setParameter('maxPrice', $maxPrice);
+        }
+        if (!empty($sizes)) {
+            $orX = $qb->expr()->orX();
+            foreach ($sizes as $size) {
+                $orX->add($qb->expr()->like('p.sizes', $qb->expr()->literal('%' . $size . '%')));
+            }
+            $qb->andWhere($orX);
+        }
+        if (!empty($colors)) {
+            $orX = $qb->expr()->orX();
+            foreach ($colors as $color) {
+                $orX->add($qb->expr()->like('p.colors', $qb->expr()->literal('%' . $color . '%')));
+            }
+            $qb->andWhere($orX);
+        }
         $query = $qb->getQuery();
         $products = $query->getResult();
 
@@ -34,9 +67,9 @@ class ProductController extends AbstractController
         $productsOpinions = $query->getResult();
 
 
-
         foreach ($products as $key => $product) {
             $products[$key]['averageRating'] = 0;
+            $products[$key]['averageRatingSum'] = 0;
             $products[$key]['opinionsCount'] = 0;
             foreach ($productsOpinions as $opinion) {
                 if ($product['id'] == $opinion['idProduct']) {
@@ -46,9 +79,19 @@ class ProductController extends AbstractController
             }
             if ($products[$key]['opinionsCount'] > 0) {
                 $products[$key]['rating'] = number_format($products[$key]['averageRating'] / $products[$key]['opinionsCount'], 1, '.', '') . ' (' . $products[$key]['opinionsCount'] . ')';
+                $products[$key]['averageRatingSum'] = $products[$key]['averageRating'] / $products[$key]['opinionsCount'];
             } else {
                 $products[$key]['rating'] = '0 (0)';
             }
+        }
+        if ($sortBy === 'popular_desc') {
+            usort($products, function ($a, $b) {
+                return $b['opinionsCount'] <=> $a['opinionsCount'];
+            });
+        } else if ($sortBy === 'rating_desc') {
+            usort($products, function ($a, $b) {
+                return $b['averageRatingSum'] <=> $a['averageRatingSum'];
+            });
         }
         return $this->render('product/list.html.twig', [
             'products' => $products
